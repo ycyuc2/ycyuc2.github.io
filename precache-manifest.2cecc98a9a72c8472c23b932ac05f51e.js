2b835aefde90dc621615f64786057905458e6ed3
Mark <mark@egforyou.com>
2019-08-04 00:41:25 +0800

    first commit

---
+++ precache-manifest.2cecc98a9a72c8472c23b932ac05f51e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f13bc2088d520b03d7c2c749807056d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "549c823e2510da8b3065",
+    "url": "/static/css/2.c6231af7.chunk.css"
+  },
+  {
+    "revision": "0a999092d14cd69739e5",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "549c823e2510da8b3065",
+    "url": "/static/js/2.6c4db9c3.chunk.js"
+  },
+  {
+    "revision": "0a999092d14cd69739e5",
+    "url": "/static/js/main.b30d12de.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
