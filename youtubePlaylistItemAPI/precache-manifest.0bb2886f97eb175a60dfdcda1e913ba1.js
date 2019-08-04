dcaef36b44570ac2eaa0868be75bfd6dc5365cf6
Mark <mark@egforyou.com>
2019-08-04 22:18:05 +0800

    youtube playlistitem apt

---
+++ youtubePlaylistItemAPI/precache-manifest.0bb2886f97eb175a60dfdcda1e913ba1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d20cacbfb4db649b6a3fae90b3798ae",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a850627d71ef6be7b366",
+    "url": "./static/js/2.9173320f.chunk.js"
+  },
+  {
+    "revision": "78c9c3ab0aa86b48cd59",
+    "url": "./static/js/main.a7fba239.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
